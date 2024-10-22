4c54efcd3e65f538f5af6556738bfd5a710967e1
samson <samsonzacharia1973@gmail.com>
2024-10-22 16:51:42 +0530

    incomeplete -profile page setup done , update credit pending

--- app/(root)/credits/page.jsx
+++ app/(root)/credits/page.jsx
@@ -10,12 +10,14 @@ import { plans } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
 import Checkout from "@/components/shared/Checkout";
 
+
 const Credits = async () => {
+
   const { userId } = auth();
-
   if (!userId) redirect("/sign-in");
-
   const user = await getUserById(userId);
+
+
 
   return (
     <>
--- components/shared/Checkout.jsx
+++ components/shared/Checkout.jsx
@@ -1,20 +1,28 @@
+
 import {getaccesstoken} from '@/lib/actions/transaction.actions'
 import { CheckoutCredits } from "@/lib/actions/transaction.actions";
 import { Button } from "../ui/button";
-const Checkout = ({plan,amount,credits,buyerId}) => { 
+import { redirect } from 'next/navigation';
+const Checkout = async({plan,amount,credits,buyerId}) => { 
+
+
     const onCheckout = async()=>{
         'use server'
-        const token=await getaccesstoken();
-        const transaction={
+    const token=await getaccesstoken('post');
+    const transaction={
             plan,
             amount,
             credits,
             buyerId
         }
-        await CheckoutCredits(transaction,token.data.access_token);
+    const url=await CheckoutCredits(transaction,token.data.access_token); 
+    
+    if(url) redirect(url);     
+     
+   
     }
     return (
-        <form action={onCheckout} method="POST">
+        <form action={onCheckout} >
             <section>
                 <Button
                     type="submit"
@@ -32,79 +40,3 @@ export default Checkout
 
 
 
-// "use client";
-
-// import { loadStripe } from "@stripe/stripe-js";
-// import { useEffect } from "react";
-
-// import { useToast } from "@/components/ui/use-toast";
-// import { checkoutCredits } from "@/lib/actions/transaction.action";
-
-// import { Button } from "../ui/button";
-
-// const Checkout = ({
-//   plan,
-//   amount,
-//   credits,
-//   buyerId,
-// }: {
-//   plan: string;
-//   amount: number;
-//   credits: number;
-//   buyerId: string;
-// }) => {
-//   const { toast } = useToast();
-
-//   useEffect(() => {
-//     loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-//   }, []);
-
-//   useEffect(() => {
-//     // Check to see if this is a redirect back from Checkout
-//     const query = new URLSearchParams(window.location.search);
-//     if (query.get("success")) {
-//       toast({
-//         title: "Order placed!",
-//         description: "You will receive an email confirmation",
-//         duration: 5000,
-//         className: "success-toast",
-//       });
-//     }
-
-//     if (query.get("canceled")) {
-//       toast({
-//         title: "Order canceled!",
-//         description: "Continue to shop around and checkout when you're ready",
-//         duration: 5000,
-//         className: "error-toast",
-//       });
-//     }
-//   }, []);
-
-//   const onCheckout = async () => {
-//     const transaction = {
-//       plan,
-//       amount,
-//       credits,
-//       buyerId,
-//     };
-
-//     await checkoutCredits(transaction);
-//   };
-
-//   return (
-{/* <form action={onCheckout} method="POST">
-<section>
-<Button
-    type="submit"
-    role="link"
-    className="w-full rounded-full bg-purple-gradient bg-cover"
->
-    Buy Credit
-</Button>
-</section>
-</form> */}
-//   );
-// };
-
-// export default Checkout;
